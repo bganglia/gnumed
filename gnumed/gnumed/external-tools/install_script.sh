ff2d073292506c60d04dca16124a7442ea737472
bganglia <bganglia892@gmail.com>
2020-04-16 20:42:12 -0400

    add install script

---
+++ gnumed/gnumed/external-tools/install_script.sh
@@ -0,0 +1,20 @@
+conda install wxpython
+conda install -c anaconda psycopg2
+conda install -c anaconda psutil
+conda install -c anaconda docutils
+conda install -c anaconda unidecode
+conda install -c conda-forge pysvg-py3
+conda install -c conda-forge vobject
+conda install -c conda-forge httplib2
+#Why is this so hard?
+sudo apt-get install python-libxml2
+#add to conda-forge?
+pip install pyenchant 
+pip install hl7
+pip install PyQRCode
+pip install pyudev
+pip install humblewx
+#only on windows. Does not work on this computer, _which is OK_
+pip install twain
+#GNUmed Python modules... 
+
