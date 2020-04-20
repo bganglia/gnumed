9202c9a1bc95d2135b1b8cbef09070e2d530afc4
bganglia <bganglia892@gmail.com>
2020-04-20 01:13:30 -0500

    install packages for both Python 3.7 and 3 (necessary?)

--- gnumed/gnumed/external-tools/install_script.sh
+++ gnumed/gnumed/external-tools/install_script.sh
@@ -3,22 +3,36 @@ sudo apt-get install python-psycopg2
 sudo apt-get install libpq-dev
 sudo apt-get install python3-pip
 sudo apt-get install libgtk-3-dev
-pip3 install wxpython
-pip3 install psycopg2
-pip3 install psutil
-pip3 install docutils
-pip3 install unidecode
-pip3 install pysvg-py3
-pip3 install vobject
-pip3 install httplib2
+python3.7 -m pip install wxpython
+python3 -m pip install wxpython
+python3.7 -m pip install psycopg2
+python3 -m pip install psycopg2
+python3.7 -m pip install psutil
+python3 -m pip install psutil
+python3.7 -m pip install docutils
+python3 -m pip install docutils
+python3.7 -m pip install unidecode
+python3 -m pip install unidecode
+python3.7 -m pip install pysvg-py3
+python3 -m pip install pysvg-py3
+python3.7 -m pip install vobject
+python3 -m pip install vobject
+python3.7 -m pip install httplib2
+python3 -m pip install httplib2
 #Why is this so hard?
 sudo apt-get install python-libxml2
 #add to conda-forge?
-pip3 install pyenchant
-pip3 install hl7
-pip3 install PyQRCode
-pip3 install pyudev
-pip3 install humblewx
+python3.7 -m pip install pyenchant
+python3 -m pip install pyenchant
+python3.7 -m pip install hl7
+python3 -m pip install hl7
+python3.7 -m pip install PyQRCode
+python3 -m pip install PyQRCode
+python3.7 -m pip install pyudev
+python3 -m pip install pyudev
+python3.7 -m pip install humblewx
+python3 -m pip install humblewx
 #only on windows. Does not work on this computer, _which is OK_
-pip3 install twain
+python3.7 -m pip install twain
+python3 -m pip install twain
 #GNUmed Python modules...
