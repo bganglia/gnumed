cd3dff784a56830271a252301d9da2d538c0a73e
bganglia <bganglia892@gmail.com>
2020-04-20 00:43:16 -0500

    finalize install script

--- gnumed/gnumed/external-tools/install_script.sh
+++ gnumed/gnumed/external-tools/install_script.sh
@@ -1,20 +1,24 @@
-conda install wxpython
-conda install -c anaconda psycopg2
-conda install -c anaconda psutil
-conda install -c anaconda docutils
-conda install -c anaconda unidecode
-conda install -c conda-forge pysvg-py3
-conda install -c conda-forge vobject
-conda install -c conda-forge httplib2
+sudo apt-get install postgresql postgresql-contrib
+sudo apt-get install python-psycopg2
+sudo apt-get install libpq-dev
+sudo apt-get install python3-pip
+sudo apt-get install libgtk-3-dev
+pip3 install wxpython
+pip3 install psycopg2
+pip3 install psutil
+pip3 install docutils
+pip3 install unidecode
+pip3 install pysvg-py3
+pip3 install vobject
+pip3 install httplib2
 #Why is this so hard?
 sudo apt-get install python-libxml2
 #add to conda-forge?
-pip install pyenchant 
-pip install hl7
-pip install PyQRCode
-pip install pyudev
-pip install humblewx
+pip3 install pyenchant
+pip3 install hl7
+pip3 install PyQRCode
+pip3 install pyudev
+pip3 install humblewx
 #only on windows. Does not work on this computer, _which is OK_
-pip install twain
-#GNUmed Python modules... 
-
+pip3 install twain
+#GNUmed Python modules...
