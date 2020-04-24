d792ef28f106584709c2ade27d8c85d0ac030b9b
bganglia <48276939+bganglia@users.noreply.github.com>
2020-04-24 15:39:37 -0400

    tidy up script

--- gnumed/gnumed/external-tools/install_script.sh
+++ gnumed/gnumed/external-tools/install_script.sh
@@ -3,6 +3,8 @@ sudo apt-get install python-psycopg2
 sudo apt-get install libpq-dev
 sudo apt-get install python3-pip
 sudo apt-get install libgtk-3-dev
+sudo apt-get install python-libxml2
+
 python3.7 -m pip install wxpython
 python3 -m pip install wxpython
 python3.7 -m pip install psycopg2
@@ -19,9 +21,7 @@ python3.7 -m pip install vobject
 python3 -m pip install vobject
 python3.7 -m pip install httplib2
 python3 -m pip install httplib2
-#Why is this so hard?
-sudo apt-get install python-libxml2
-#add to conda-forge?
+
 python3.7 -m pip install pyenchant
 python3 -m pip install pyenchant
 python3.7 -m pip install hl7
@@ -32,7 +32,7 @@ python3.7 -m pip install pyudev
 python3 -m pip install pyudev
 python3.7 -m pip install humblewx
 python3 -m pip install humblewx
+
 #only on windows. Does not work on this computer, _which is OK_
 python3.7 -m pip install twain
 python3 -m pip install twain
-#GNUmed Python modules...
